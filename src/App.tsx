da64e0db6ebe4a87d64a473d1aceb42538466cd3
SeoJunhwan <seojunhwan@kakao.com>
2022-01-29 21:53:43 +0900

    ➕🎨 ES Lint, Prettier 설정
    원활한 협업을 위해 정적 코드 분석 도구 ES Lint, 일관된 코드 포메팅을 위한 Prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import Login from './Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <Router>
-    <Routes>
-    <Route path="/" element={<Login />} />
-    </Routes>
-  </Router>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/Hooks/useInput.ts
+++ src/Hooks/useInput.ts
@@ -1,17 +1,17 @@
-import React, { useState } from "react"
+import React, { useState } from 'react';
 
 type EventHandler = React.ChangeEventHandler<HTMLInputElement>;
 
-const useInput = (initialValue?:string) : [string, EventHandler] => {
-    const [value, setValue] = useState(initialValue ?? "");
-    const onChange = (event : React.FormEvent<HTMLInputElement>) =>{
-        const {
-            currentTarget: {value}
-        } = event;
-        setValue(value);
-    }
+const useInput = (initialValue?: string): [string, EventHandler] => {
+  const [value, setValue] = useState(initialValue ?? '');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
 
-    return [value, onChange];
-}
+  return [value, onChange];
+};
 
 export default useInput;
