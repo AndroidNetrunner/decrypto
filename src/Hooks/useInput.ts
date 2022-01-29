8f70dd5a636afaad8914a5a67b2a8ad22f10d5a9
AndroidNetrunner <kby9906@naver.com>
2022-01-29 18:06:06 +0900

    [ADD] Login Page

---
+++ src/Hooks/useInput.ts
@@ -0,0 +1,17 @@
+import React, { useState } from "react"
+
+type EventHandler = React.ChangeEventHandler<HTMLInputElement>;
+
+const useInput = (initialValue?:string) : [string, EventHandler] => {
+    const [value, setValue] = useState(initialValue ?? "");
+    const onChange = (event : React.FormEvent<HTMLInputElement>) =>{
+        const {
+            currentTarget: {value}
+        } = event;
+        setValue(value);
+    }
+
+    return [value, onChange];
+}
+
+export default useInput;
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,7 +1,12 @@
-import { useState } from 'react'
-import styled, { css } from "styled-components"
+import styled from "styled-components"
+import React, { useState } from "react"
+import useInput from './Hooks/useInput';
 
-function Login() {
+
+export default function Login() {
+    const [nickname, onChangeNickname] = useInput();
+    const [roomNumber, onChangeRoomNumber] = useInput();
+
     return (
         <Container>
             <Title>
@@ -10,24 +15,52 @@ function Login() {
             <Form>
                 <InputContainer>
                     <label htmlFor="nickname">닉네임</label>
-                    <input id="nickname" />
+                    <input value={nickname} onChange={onChangeNickname} id="nickname" />
                     <label htmlFor='room'>방 번호</label>
-                    <input id='room'/>
+                    <input value={roomNumber} onChange={onChangeRoomNumber} id='room' />
                 </InputContainer>
                 <ButtonContainer>
-                    <Button>방 만들기</Button>
-                    <Button>참가하기</Button>
+                    <EntryButton disabled={!nickname.length}>방 만들기</EntryButton>
+                    <EntryButton disabled={!(nickname.length && roomNumber.length)}>참가하기</EntryButton>
                 </ButtonContainer>
             </Form>
         </Container>
     )
 }
+
+// 둘 다 값이 없을 경우 => 방 만들기, 참가하기 비활성화 
+// 닉네임에만 값이 있을 경우 -> 방 만들기 버튼 활성화, 참가하기 버튼 비활성화
+// 닉네임과 방 번호에 값이 있을 경우 -> 참가하기 버튼 활성화
+
 // 제목
 // Input 2개
+
 const Button = styled.button`
+    &:disabled{
+        pointer-events: none;
+        opacity: 0.65;
+    }
+    display: inline-block;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #FFFFFF;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: middle;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    border-radius: 0.25rem;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+`;
+
+const EntryButton = styled(Button)`
     background-color: purple;
-    color: white;
-    margin: 4px;
 `;
 
 const InputContainer = styled.div`
@@ -37,7 +70,6 @@ const InputContainer = styled.div`
 
 const ButtonContainer = styled.div`
     dispaly:flex;
-
 `;
 
 const Form = styled.form`
@@ -55,4 +87,3 @@ const Container = styled.div`
     max-width:860px;
     margin:auto;
 `
-export default Login;
