e2208a12d07dff6d872ab8e0b2ac39d6000b72df
SeoJunhwan <seojunhwan@kakao.com>
2022-01-31 15:41:42 +0900

    📰✨ Game Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BaseLayout from './Layouts/BaseLayout';
+import GameLayout from './Layouts/GameLayout';
 import Login from './Login';
+import Game from './Routes/Game';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='game' element={<GameLayout />}>
+          <Route path=':roomId' element={<Game />} />
+        </Route>
+        <Route path='/' element={<BaseLayout />}>
+          <Route index element={<Login />} />
+        </Route>
       </Routes>
     </Router>
   );
