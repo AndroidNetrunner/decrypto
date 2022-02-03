cd26f9bd7751137daddff2112400d8497851a32d
SeoJunhwan <seojunhwan@kakao.com>
2022-02-03 21:54:38 +0900

    ♻️ Team Join & Change 로직 리팩토링
    기존 팀에 따라 다르게 동작하는 코드를 팀 상태를 확인 후
    유기적으로 동작하게끔 리팩토링 진행

--- src/Pages/Game/index.tsx
+++ src/Pages/Game/index.tsx
@@ -27,8 +27,8 @@ const dummySecondTeam = [
     nickname: 'yeoyoon',
   },
   {
-    userId: 132,
-    nickname: 'juseo',
+    userId: 394998,
+    nickname: 'gamja',
   },
   {
     userId: 122,
@@ -36,9 +36,17 @@ const dummySecondTeam = [
   },
 ];
 
-interface ITempUser {
-  nickname: string;
+interface IUser {
   userId: number;
+  nickname: string;
+}
+interface Iteam {
+  firstTeam: {
+    users: IUser[];
+  };
+  secondTeam: {
+    users: IUser[];
+  };
 }
 
 export default function Game() {
@@ -46,10 +54,18 @@ export default function Game() {
     firstTeamName: 'White',
     secondTeamName: 'Black',
   });
-  const [firstTeam, setFirstTeam] = useState<ITempUser[]>(dummyFirstTeam);
-  const [secondTeam, setSecondTeam] = useState<ITempUser[]>(dummySecondTeam);
+
+  const [team, setTeam] = useState<Iteam>({
+    firstTeam: {
+      users: [...dummyFirstTeam],
+    },
+    secondTeam: {
+      users: [...dummySecondTeam],
+    },
+  });
 
   const { firstTeamName, secondTeamName } = teamNames;
+  const { firstTeam, secondTeam } = team;
 
   const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = event.target;
@@ -61,34 +77,20 @@ export default function Game() {
 
   const onClickJoinButton = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
-      currentTarget: { name },
+      currentTarget: { name: teamName },
     } = event;
 
-    if (name === 'firstTeam') {
-      const targetIndex = firstTeam.findIndex((user) => user.userId === dummyUser.userId);
-      if (targetIndex === -1) {
-        if (firstTeam.length === 4) {
-          alert('인원이 이상합니다!');
-          return;
-        }
-        const filterdUser = secondTeam.filter((user) => user.userId !== dummyUser.userId);
-        setSecondTeam(filterdUser);
-        setFirstTeam([...firstTeam, dummyUser]);
+    if (teamName === 'firstTeam' || teamName === 'secondTeam') {
+      const isInclude = team[teamName].users.some((user) => user.userId === dummyUser.userId);
+      if (!isInclude && team[teamName].users.length !== 4) {
+        const filterdUsers = {
+          firstTeam: { users: firstTeam.users.filter((user) => user.userId !== dummyUser.userId) },
+          secondTeam: { users: secondTeam.users.filter((user) => user.userId !== dummyUser.userId) },
+        };
+        setTeam({ ...team, ...filterdUsers, [teamName]: { users: [...team[teamName].users, dummyUser] } });
       } else {
-        setFirstTeam([...firstTeam.slice(0, targetIndex), ...firstTeam.slice(targetIndex + 1)]);
-      }
-    } else if (name === 'secondTeam') {
-      const targetIndex = secondTeam.findIndex((user) => user.userId === dummyUser.userId);
-      if (targetIndex === -1) {
-        if (secondTeam.length === 4) {
-          alert('인원이 이상합니다!');
-          return;
-        }
-        const filterdUser = firstTeam.filter((user) => user.userId !== dummyUser.userId);
-        setFirstTeam(filterdUser);
-        setSecondTeam([...secondTeam, dummyUser]);
-      } else {
-        setSecondTeam([...secondTeam.slice(0, targetIndex), ...secondTeam.slice(targetIndex + 1)]);
+        const filterdUsers = team[teamName].users.filter((user) => user.userId !== dummyUser.userId);
+        setTeam({ ...team, [teamName]: { users: [...filterdUsers] } });
       }
     }
   };
@@ -98,8 +100,8 @@ export default function Game() {
       <TeamContainer>
         <input name='firstTeamName' value={firstTeamName} onChange={onChangeName} type='text' />
         <UserList>
-          {firstTeam.length ? (
-            firstTeam.map((user) => <User key={user.userId}>{user.nickname}</User>)
+          {firstTeam.users.length ? (
+            firstTeam.users.map((user) => <User key={user.userId}>{user.nickname}</User>)
           ) : (
             <User>참가하세오,,!</User>
           )}
@@ -108,17 +110,21 @@ export default function Game() {
           type='button'
           name='firstTeam'
           onClick={onClickJoinButton}
-          isFull={firstTeam.length === 4}
+          isFull={firstTeam.users.length === 4}
         >
-          {firstTeam.includes(dummyUser) ? '팀 떠나기' : firstTeam.length === 4 ? 'Full' : '팀 참가하기'}
+          {firstTeam.users.includes(dummyUser)
+            ? '팀 떠나기'
+            : firstTeam.users.length === 4
+            ? 'Full'
+            : '팀 참가하기'}
         </JoinButton>
       </TeamContainer>
 
       <TeamContainer>
         <input name='secondTeamName' value={secondTeamName} onChange={onChangeName} type='text' />
         <UserList>
-          {secondTeam.length ? (
-            secondTeam.map((user) => <User key={user.userId}>{user.nickname}</User>)
+          {secondTeam.users.length ? (
+            secondTeam.users.map((user) => <User key={user.userId}>{user.nickname}</User>)
           ) : (
             <User>참가하세오,,!</User>
           )}
@@ -128,9 +134,13 @@ export default function Game() {
           type='button'
           name='secondTeam'
           onClick={onClickJoinButton}
-          isFull={secondTeam.length === 4}
+          isFull={secondTeam.users.length === 4}
         >
-          {secondTeam.includes(dummyUser) ? '팀 떠나기' : secondTeam.length === 4 ? 'Full' : '팀 참가하기'}
+          {secondTeam.users.includes(dummyUser)
+            ? '팀 떠나기'
+            : secondTeam.users.length === 4
+            ? 'Full'
+            : '팀 참가하기'}
         </JoinButton>
       </TeamContainer>
     </Container>
@@ -185,7 +195,6 @@ const JoinButton = styled.button<{ isFull: boolean }>`
   font-size: 1.6rem;
   color: white;
   width: 100%;
-  /* background-color: rgba(0, 0, 128, 0.6); */
   background-color: ${(props) => (props.isFull ? '#ff9999' : 'rgba(0, 0, 128, 0.6)')};
   border: none;
   padding: 1rem 0rem;
