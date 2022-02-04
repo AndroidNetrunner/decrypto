081f409ec359b3012130d9c22091a08088af2bed
AndroidNetrunner <kby9906@naver.com>
2022-02-04 15:50:13 +0900

    [ADD] 점수 표시와 상대방 힌트 목록 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Login from './Login'
+import OpponentHints from './components/OpponentHints'
+import ScoreTable from './components/ScoreTable'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +13,8 @@ function App() {
   <Router>
     <Routes>
     <Route path="/" element={<Login />} />
+    <Route path="/hidden" element={<OpponentHints hints={[['폭포','나무'], ['꿈', '시계'], ['사랑','태양'], ['눈','시련']]}/>} />
+    <Route path="/scoreTable" element={<ScoreTable hints={[['폭포','나무'], ['꿈', '시계'], ['사랑','태양'], ['눈','시련']]}/>} />
     </Routes>
   </Router>
   )
