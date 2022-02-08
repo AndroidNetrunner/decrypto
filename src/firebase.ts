c8541e4500f6a94539978568adad53d20e247963
SeoJunhwan <seojunhwan@kakao.com>
2022-02-08 15:22:07 +0900

    ➕ Install Firebase
    서버리스 구현을 위해 Firebase 도입

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCauAH_OoASaUBHqQBwZ_DUi7HhZBKdOBI',
+  authDomain: 'decrypto-16ad6.firebaseapp.com',
+  projectId: 'decrypto-16ad6',
+  storageBucket: 'decrypto-16ad6.appspot.com',
+  messagingSenderId: '1021376643170',
+  appId: '1:1021376643170:web:4c92929a3e761eb23d913b',
+  measurementId: 'G-ZTS5LFWJ36',
+};
+
+const app = initializeApp(firebaseConfig);
