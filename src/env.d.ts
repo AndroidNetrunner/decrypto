d43454c97af4c416cec19574cd076b0d788f3642
SeoJunhwan <seojunhwan@kakao.com>
2022-02-14 22:12:48 +0900

    🔧 환경변수에 DATABASE_URL 추가
    환경변수에 DATABASE_URL 추가합니다.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 interface ImportMetaEnv {
   readonly REACT_APP_API_KEY: string;
   readonly REACT_APP_AUTH_DOMAIN: string;
+  readonly REACT_APP_DATABASE_URL: string;
   readonly REACT_APP_PROJECT_ID: string;
   readonly REACT_APP_STORAGE_BUCKET: string;
   readonly REACT_APP_MESSAGING_SENDER_ID: string;
