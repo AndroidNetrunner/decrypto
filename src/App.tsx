1f86f6a429bcc9f5ac981cf135fa2d23332fa9c1
AndroidNetrunner <kby9906@naver.com>
2022-02-15 15:02:08 +0900

    [ADD] Hintsubmit Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Login from './Login'
-import OpponentHints from './components/OpponentHints'
-import ScoreTable from './components/ScoreTable'
-
+import OpponentHints from './components/OpponentHints';
+import ScoreTable from './components/ScoreTable';
+import HintSubmit from './components/HintSubmit';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,7 +14,7 @@ function App() {
     <Routes>
     <Route path="/" element={<Login />} />
     <Route path="/hidden" element={<OpponentHints hints={[['폭포','나무'], ['꿈', '시계'], ['사랑','태양'], ['눈','시련']]}/>} />
-    <Route path="/scoreTable" element={<ScoreTable hints={[['폭포','나무'], ['꿈', '시계'], ['사랑','태양'], ['눈','시련']]}/>} />
+    <Route path="/test" element={<HintSubmit answer={[2, 1, 4]} wordList={['평화', '자유', '사랑', 'My life']} />} />
     </Routes>
   </Router>
   )
