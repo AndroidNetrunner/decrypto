1f663595dff9c7913b6df5c118af6dd4d49bfffe
071yoon <yeonggilol@gmail.com>
2022-02-17 17:51:19 +0900

    Timer 컴포넌트에 시간 나오도록 수정 완료 시간에 걸맞는 폰트 추가 완료

--- src/Components/Timer.tsx
+++ src/Components/Timer.tsx
@@ -1,21 +1,24 @@
 import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import '../fonts/font.css';
 
 export default function Timer({ gameTime }: { gameTime: number }) {
   const progressRef = useRef<HTMLDivElement>(null);
+  const progressValueRef = useRef<HTMLDivElement>(null);
 
   let progressValue = 0;
   const progressEndValue = 100;
   const setTimer = gameTime * 10;
   const progress = setInterval(() => {
-    if (progressRef.current !== null) {
+    if (progressRef.current !== null && progressValueRef.current !== null) {
       progressValue += 1;
+      progressValueRef.current.textContent = `${(gameTime - (progressValue / 100) * gameTime).toFixed(0)}`;
       progressRef.current.style.background = `conic-gradient(
         #a37094 ${progressValue * 3.6}deg,
         #ccc2c9 ${progressValue * 3.6}deg
         )`;
       if (progressValue === progressEndValue) {
-        console.log('done'); //추후에 서버에 제출하는 코드로 변경
+        console.log('done'); // 추후에 서버에 제출하는 코드로 변경
         clearInterval(progress);
       }
     }
@@ -25,8 +28,23 @@ export default function Timer({ gameTime }: { gameTime: number }) {
     return () => clearInterval(progress);
   }, []);
 
-  return <CircularProgress ref={progressRef} />;
+  return (
+    <Container>
+      <TimerLeft ref={progressValueRef}>{(setTimer / 10).toFixed(0)}</TimerLeft>
+      <CircularProgress ref={progressRef} />
+    </Container>
+  );
 }
+
+const Container = styled.div``;
+
+const TimerLeft = styled.div`
+  font-family: neodgm;
+  font-size: 10rem;
+  text-align: center;
+  height: 100px;
+  width: 250px;
+`;
 
 const CircularProgress = styled.div`
   position: relative;
