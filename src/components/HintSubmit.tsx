c17887609ae7d79210233426bf28bf06019c68d2
byukim <kby9906@naver.com>
2022-02-21 14:30:46 +0900

    [ADD] HintSubmit, README.md

--- src/components/HintSubmit.tsx
+++ src/components/HintSubmit.tsx
@@ -6,19 +6,38 @@ type ourTeamWord = [string, string, string, string];
 function HintSubmit({ answer, wordList }: { answer: answerCode; wordList: ourTeamWord }) {
   return (
     <>
-      <span>
+      <AnswerCode>
         정답 코드: {answer[0]} - {answer[1]} - {answer[2]}
-      </span>
+      </AnswerCode>
+      <br />
+      <br />
       <HintForm>
-        <input type='text' placeholder={`${wordList[answer[0] - 1]}의 힌트...`} />
-        <input type='text' placeholder={`${wordList[answer[1] - 1]}의 힌트...`} />
-        <input type='text' placeholder={`${wordList[answer[2] - 1]}의 힌트...`} />
+        <label htmlFor='hintform-first'>#{answer[0]}</label>
+        <input type='text' id='hintform-first' placeholder={`${wordList[answer[0] - 1]}의 힌트...`} /> <br />
+        <label htmlFor='hintform-second'>#{answer[1]}</label>
+        <input type='text' id='hintform-second' placeholder={`${wordList[answer[1] - 1]}의 힌트...`} /> <br />
+        <label htmlFor='hintform-third'>#{answer[2]}</label>
+        <input type='text' id='hintform-third' placeholder={`${wordList[answer[2] - 1]}의 힌트...`} /> <br />
         <input type='submit' value='제출' />
       </HintForm>
     </>
   );
 }
 
-const HintForm = styled.form``;
+const HintForm = styled.form`
+  label {
+    font-size: 15pt;
+    font-weight: bold;
+  }
+  input {
+    margin-bottom: 1rem;
+    margin-left: 0.5rem;
+  }
+`;
+
+const AnswerCode = styled.span`
+  font-size: 15pt;
+  font-weight: bold;
+`;
 
 export default HintSubmit;
