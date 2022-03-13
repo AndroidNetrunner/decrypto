5a41dca37dcb5e8498a6c02042cee3a069c1ef40
071yoon <yeonggilol@gmail.com>
2022-03-13 19:11:12 +0900

    [FIXED] WordList map 으로 구현 및 정렬 잘 되게 수정정

--- src/Pages/Game/Components/Word.tsx
+++ src/Pages/Game/Components/Word.tsx
@@ -1,40 +1,44 @@
 import styled from 'styled-components';
 import TV from '../../../Assets/img/tv-fullsz.gif';
 
-function Word({ wordList }: { wordList: [string, string, string, string] }) {
+interface Props {
+  wordList: [string, string, string, string];
+}
+
+function Word({ wordList }: Props) {
   return (
     <Container>
-      <ItemList>
-        <Number>1</Number>
-        <Number>2</Number>
-        <Number>3</Number>
-        <Number>4</Number>
-      </ItemList>
-      <ItemList>
-        <TmpContainer>{wordList[0]}</TmpContainer>
-        <TmpContainer>{wordList[1]}</TmpContainer>
-        <TmpContainer>{wordList[2]}</TmpContainer>
-        <TmpContainer>{wordList[3]}</TmpContainer>
-      </ItemList>
+      {wordList.map((word, index) => (
+        <div>
+          <Number>{index + 1}</Number>
+          <WordContainer>
+            <div>{word}</div>
+          </WordContainer>
+        </div>
+      ))}
     </Container>
   );
 }
 
-const TmpContainer = styled.div`
-  width: 18rem;
-  height: 13rem;
-  font-size: 250%;
+const WordContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 15rem;
+  height: 11rem;
+  font-size: 175%;
   background: url(${TV});
   background-size: cover;
   text-align: center;
-  padding: 4% 7% 5% 4%;
+  div {
+    margin-right: 2rem;
+    padding-bottom: 0.5rem;
+  }
 `;
 const Container = styled.div`
   padding: 20px;
   width: 100%;
-`;
-
-const ItemList = styled.div`
   display: flex;
   justify-content: space-evenly;
 `;
@@ -43,7 +47,6 @@ const Number = styled.div`
   font-size: 4rem;
   font-weight: bold;
   text-align: center;
-  width: 100px;
 `;
 
 export default Word;
