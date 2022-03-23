e52979a4fc2336c72fd68ce9c4e37f676e45d22f
SeoJunhwan <seojunhwan@kakao.com>
2022-03-24 01:16:58 +0900

    🔥 불필요 환경변수 인터페이스 삭제

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,12 +1,9 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
       MONGO_DB_URL: string;
       COOKIE_SECRET: string;
-      NODE_ENV: 'development' | 'production';
-      GITHUB_CLIENT_ID: string;
-      GITHUB_CLIENT_SECRET: string;
-      CLIENT_URL: string;
     }
   }
 }
