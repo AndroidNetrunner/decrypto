9810c52f3175b0146254017627354aadca7e6b69
AndroidNetrunner <kby9906@naver.com>
2022-03-29 12:28:43 +0900

    [FIX] 두 번째 세 번째 단어에서도 1, 2, 3, 4가 출력되도록 수정

--- src/Pages/Game/Components/CodeGuess.tsx
+++ src/Pages/Game/Components/CodeGuess.tsx
@@ -2,46 +2,7 @@ import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../../Redux/store/rootStore';
 import socket from '../../../Utils/socket';
-
-function makeEmptyOptionTag() {
-  const emptyOptionTag = document.createElement('option');
-  emptyOptionTag.value = '';
-  emptyOptionTag.disabled = true;
-  emptyOptionTag.selected = true;
-  return emptyOptionTag;
-}
-
-function makeNewOptions(code: HTMLSelectElement, possibleCode: number[], codeIndex: number) {
-  while (code.firstChild) {
-    code.removeChild(code.firstChild);
-  }
-  code.appendChild(makeEmptyOptionTag());
-  possibleCode.forEach((number, index) => {
-    const child = document.createElement('option');
-    child.value = number.toString();
-    child.text = number.toString();
-    if (codeIndex !== -1 && codeIndex === index + 1) {
-      child.selected = true;
-    }
-    code.appendChild(child);
-  });
-}
-
-function handleChange() {
-  const firstCode = document.getElementById('guessFirstCode') as HTMLSelectElement;
-  const secondCode = document.getElementById('guessSecondCode') as HTMLSelectElement;
-  const thirdCode = document.getElementById('guessThirdCode') as HTMLSelectElement;
-  const possibleCode = [1, 2, 3, 4];
-  const selectedIndex = [
-    firstCode.options.selectedIndex,
-    secondCode.options.selectedIndex,
-    thirdCode.options.selectedIndex,
-  ];
-  const possibleSecondCode = possibleCode.filter((value, index) => index !== selectedIndex[0] - 1);
-  const possibleThirdCode = possibleSecondCode.filter((value, index) => index !== selectedIndex[1] - 1);
-  makeNewOptions(secondCode, possibleSecondCode, selectedIndex[1]);
-  makeNewOptions(thirdCode, possibleThirdCode, selectedIndex[2]);
-}
+import Game from '../../../Interfaces/Game.interface';
 
 function CodeGuess() {
   const { stageNumber } = useSelector((rootState: RootState) => rootState.game);
@@ -58,13 +19,16 @@ function CodeGuess() {
     const secondCode = Number(secondSelect.options[secondSelect.selectedIndex].value);
     const thirdSelect = document.getElementById('guessThirdCode') as HTMLSelectElement;
     const thirdCode = Number(thirdSelect.options[thirdSelect.selectedIndex].value);
-    socket.emit('SUBMIT_CODE', [firstCode, secondCode, thirdCode], (gameInfo) => {
-      dispatch(gameInfo);
-    });
+    if (new Set([firstCode, secondCode, thirdCode]).size == 3)
+      socket.emit('SUBMIT_CODE', [firstCode, secondCode, thirdCode], (gameInfo: Game) => {
+        dispatch(gameInfo);
+      });
+    else
+      alert('정답 코드에는 똑같은 숫자가 존재하지 않습니다.')
   };
 
   return (
-    <GuessForm onChange={handleChange} onSubmit={handleSubmit}>
+    <GuessForm onSubmit={handleSubmit}>
       <div className='formArea'>
         <table>
           <tr>
