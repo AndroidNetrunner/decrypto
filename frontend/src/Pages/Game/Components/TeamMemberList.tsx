555ca91095272ccf320ad8bc96453676ed42269c
byukim <kby9906@naver.com>
2022-04-05 21:23:09 +0900

    [FIX] Soviet과 USA의 너비가 같도록 수정, table 태그 대신 div 태그 사용

--- frontend/src/Pages/Game/Components/TeamMemberList.tsx
+++ frontend/src/Pages/Game/Components/TeamMemberList.tsx
@@ -3,50 +3,46 @@ import styled from 'styled-components';
 import { RootState } from '../../../Redux/store/rootStore';
 import User from '../../../Interfaces/User.interface';
 
-function getRows(sovietPlayers: User[], usaPlayers: User[]) {
-  const ret = [];
-  for (let i = 0; i < Math.max(sovietPlayers.length, usaPlayers.length); i += 1) {
-    ret.push(
-      <tr>
-        <td className='Soviet' key={sovietPlayers[i] ? sovietPlayers[i].uid : `s${i}`}>
-          {sovietPlayers[i] && sovietPlayers[i].nickname}
-        </td>
-        <td className='USA' key={usaPlayers[i] ? usaPlayers[i].uid : `u${i}`}>
-          {usaPlayers[i] && usaPlayers[i].nickname}
-        </td>
-      </tr>,
-    );
-  }
-  return ret;
-}
-
 function TeamMemberList() {
   const sovietPlayers = useSelector((rootState: RootState) => rootState.game.sovietTeam.players);
   const usaPlayers = useSelector((rootState: RootState) => rootState.game.usaTeam.players);
   return (
-    <TeamTable>
-      <tr>
-        <th className='Soviet'>Soviet</th>
-        <th className='USA'>USA</th>
-      </tr>
-      {getRows(sovietPlayers, usaPlayers)}
-    </TeamTable>
+    <TeamContainer>
+      <Team color='#f15852'>
+        <div className='Soviet title'>Soviet</div>
+        {sovietPlayers.map((player: User) => (
+          <div className='Soviet'>{player.nickname}</div>
+        ))}
+      </Team>
+      <Team color='#5685d0'>
+        <div className='USA title'>USA</div>
+        {usaPlayers.map((player: User) => (
+          <div className='USA'>{player.nickname}</div>
+        ))}
+      </Team>
+    </TeamContainer>
   );
 }
 
-const TeamTable = styled.table`
-  text-align: center;
-  /* margin: auto; */
+const TeamContainer = styled.div`
+  width: 10%;
+  display: table;
+  table-layout: fixed;
+  word-break: break-all;
+`;
+
+const Team = styled.div`
   font-size: 175%;
-  .Soviet {
-    background-color: #f15852;
-  }
-  .USA {
-    background-color: #5685d0;
-  }
-  th,
-  td {
+  text-align: center;
+  display: table-cell;
+  div {
+    background-color: ${(props) => props.color};
+    display: flex;
+    flex-direction: column;
     padding: 1rem;
+  }
+  .title {
+    font-weight: bold;
   }
 `;
 
